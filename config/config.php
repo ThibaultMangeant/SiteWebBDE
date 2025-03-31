dcde1c49b44b802df341770fa8731edbf3fbe538
Thibault Mangeant <166180018+ThibaultMangeant@users.noreply.github.com>
2025-03-31 09:07:40 +0200

    Delete config/config.php

--- config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Définition de constantes pour la configuration de la base de données
-define('DB_HOST', 'woody');  // Hôte de la base de données (généralement localhost)
-define('DB_NAME', 'login');  // Nom de la base de données
-define('DB_USER', 'login');  // Nom d'utilisateur de la base de données
-define('DB_PASS', 'password');  // Mot de passe de la base de données
-define('DB_PORT', '5432');  // Mot de passe de la base de données
