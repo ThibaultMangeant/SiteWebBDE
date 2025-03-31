ce73cae304619b52eef8bf424bd09734fee20534
Julien Oyer <oyer.ludovic@gmail.com>
2025-03-31 10:39:39 +0200

    Ajouter le champ roleAutorise à l'entité Evenement et créer la classe Produit

--- app/entities/Evenement.php
+++ app/entities/Evenement.php
@@ -9,10 +9,13 @@ class Evenement
 	public function getNomEvent(): string { return $this->nomEvent; }
 	public function getDateEvent(): DateTime { return $this->dateEvent; }
 	public function getPrixEvent(): float { return $this->prixEvent; }
+	public function getRoleAutorise(): float { return $this->roleAutorise; }
+
 
 	// Setters
 	public function setIdEvent(?int $idEvent): void { $this->idEvent = $idEvent; }
 	public function setNomEvent(string $nomEvent): void { $this->nomEvent = $nomEvent; }
 	public function setDateEvent(DateTime $dateEvent): void { $this->dateEvent = $dateEvent; }
 	public function setPrixEvent(float $prixEvent): void { $this->prixEvent = $prixEvent; }
+	public function setRoleAutorise(float $roleAutorise): void { $this->roleAutorise = $roleAutorise; }
 }
