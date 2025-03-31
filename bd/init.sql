b8385d7696b49e7e0633ed51836e96c82de9bb7c
Mangeant Thibault <mangeant.thibault@gmail.com>
2025-03-31 15:35:25 +0200

    BD retermine

--- bd/init.sql
+++ bd/init.sql
@@ -8,7 +8,7 @@ INSERT INTO Produit (nomProd, qs, prixProd) VALUES
 	('Stylo', 150, 0.99);
 
 -- Insertion des évenements
-INSERT INTO Evenement (nomEvent, descEvent, dateEvent, prixEvent, roleAutoriseMin) VALUES
-	('Soirée Malevelon Creek', 'Lorem Ipsum', '2001-09-12 18:00:00', 2.0  , 'membre'),
-	('Promesse des étoiles'  , 'Lorem Ipsum', '2002-10-15 20:00:00', 0.0  , 'adherant'),
-	('Tournoi de poker'      , 'Lorem Ipsum', '2001-03-05 15:15:15', 50.00, 'adherant');
+INSERT INTO Evenement (nomEvent, descEvent, dateEvent, lieuEvent, prixEvent, roleAutoriseMin) VALUES
+	('Soirée Malevelon Creek', 'Lorem Ipsum', '2001-09-12 18:00:00', 'Quelque part', 2.0  , 'membre'),
+	('Promesse des étoiles'  , 'Lorem Ipsum', '2002-10-15 20:00:00', 'Un endroit', 0.0  , 'adherant'),
+	('Tournoi de poker'      , 'Lorem Ipsum', '2001-03-05 15:15:15', 'Où ça ?', 50.00, 'adherant');
