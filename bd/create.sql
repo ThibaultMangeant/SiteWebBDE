bfd854ee3411a2ddffc0001c38914adc5aee0523
Mangeant Thibault <mangeant.thibault@gmail.com>
2025-03-31 14:38:09 +0200

    BD termine

---
+++ bd/create.sql
@@ -0,0 +1,96 @@
+-- Suppression des tables existantes si elles existent
+DROP TABLE IF EXISTS Commande    CASCADE;
+DROP TABLE IF EXISTS Inscrit     CASCADE;
+DROP TABLE IF EXISTS Autorise    CASCADE;
+DROP TABLE IF EXISTS Produit     CASCADE;
+DROP TABLE IF EXISTS Evenement   CASCADE;
+DROP TABLE IF EXISTS "Role"      CASCADE;
+DROP TABLE IF EXISTS Utilisateur CASCADE;
+
+-- Création de la table 'Produit'
+CREATE TABLE Produit
+(
+	idProd   SERIAL        PRIMARY KEY,
+	nomProd  VARCHAR(255)  NOT NULL,
+	qs       INT           NOT NULL CHECK (qs >= 0),
+	prixProd FLOAT         NOT NULL CHECK (prixProd >= 0)
+);
+
+-- Création de la table 'Evenement'
+CREATE TABLE Evenement
+(
+	idEvent   SERIAL        PRIMARY KEY,
+	nomEvent  VARCHAR(255)  NOT NULL,
+	descEvent TEXT  NOT NULL,
+	dateEvent TIMESTAMP     NOT NULL,
+	prixEvent FLOAT         NOT NULL
+);
+
+-- Création de la table "Role"
+CREATE TABLE "Role"
+(
+	nomRole VARCHAR(10) PRIMARY KEY,
+	niveau  INT NOT NULL
+);
+
+-- Création de la table 'Utilisateur'
+CREATE TABLE Utilisateur
+(
+	netud INT PRIMARY KEY,
+	nom VARCHAR(255) NOT NULL,
+	prenom VARCHAR(255) NOT NULL,
+	tel VARCHAR(10),
+	email VARCHAR(255) NOT NULL,
+	mdp VARCHAR(255) NOT NULL,
+	typeNotification VARCHAR(10) NOT NULL CHECK (typeNotification IN ('Discord', 'Mail', 'Les deux')),
+	role VARCHAR(10) NOT NULL,
+	demande BOOLEAN NOT NULL,
+	FOREIGN KEY (role) REFERENCES "Role"(nomRole) ON DELETE CASCADE
+);
+
+-- Création de la table Commande
+CREATE TABLE Commande
+(
+	netud INT NOT NULL,
+	idProd INT NOT NULL,
+	numCommande INT NOT NULL,
+	qa INT NOT NULL CHECK (qa >= 0),
+
+	FOREIGN KEY (netud ) REFERENCES Utilisateur(netud ) ON DELETE CASCADE,
+	FOREIGN KEY (idProd) REFERENCES Produit    (idProd) ON DELETE CASCADE,
+
+	PRIMARY KEY (netud, idProd)
+);
+
+-- Création de la table Inscrit
+CREATE TABLE Inscrit
+(
+	netud INT NOT NULL,
+	idEvent INT NOT NULL,
+	note INT,
+	commentaire TEXT,
+
+	FOREIGN KEY (netud  ) REFERENCES Utilisateur(netud  ) ON DELETE CASCADE,
+	FOREIGN KEY (idEvent) REFERENCES Evenement  (idEvent) ON DELETE CASCADE,
+
+	PRIMARY KEY (netud, idEvent)
+);
+
+-- Création de la table Autorise
+CREATE TABLE Autorise
+(
+	idEvent INT NOT NULL,
+	nomRole VARCHAR(10) NOT NULL,
+
+	FOREIGN KEY (idEvent) REFERENCES Evenement(idEvent) ON DELETE CASCADE,
+	FOREIGN KEY (nomRole) REFERENCES "Role"(nomRole) ON DELETE CASCADE,
+
+	PRIMARY KEY (idEvent, nomRole)
+);
+
+-- Insertion des rôles
+INSERT INTO "Role"(nomRole, niveau) VALUES ('admin', 2), ('adherent', 1), ('membre', 0);
+
+-- Insertion d'un administrateur
+INSERT INTO Utilisateur(netud, nom, prenom, tel, email, mdp, typeNotification, role, demande) VALUES
+    (1234, 'Test', 'Johnny', '0000000000', 'test@gmail.com', '1234', 'Les deux', 'admin', FALSE);
--- bd/init.sql
+++ bd/init.sql
@@ -1,91 +1,24 @@
--- Suppression des tables existantes si elles existent
-DROP TABLE IF EXISTS Commande    CASCADE;
-DROP TABLE IF EXISTS Inscrit     CASCADE;
-DROP TABLE IF EXISTS Autorise    CASCADE;
-DROP TABLE IF EXISTS Produit     CASCADE;
-DROP TABLE IF EXISTS Evenement   CASCADE;
-DROP TABLE IF EXISTS "Role"      CASCADE;
-DROP TABLE IF EXISTS Utilisateur CASCADE;
+-- Insertion des produits
+INSERT INTO Produit (nomProd, qs, prixProd) VALUES
+	('Clé USB', 10, 5.99),
+	('T-shirt', 35, 3.99),
+	('Tasse', 20, 4.99),
+	('Gourde', 10, 5.99),
+	('Bloc-note', 25, 6.99),
+	('Stylo', 150, 0.99);
 
--- Création de la table 'Produit'
-CREATE TABLE Produit
-(
-	idProd   SERIAL        PRIMARY KEY,
-	nomProd  VARCHAR(255)  NOT NULL,
-	qs       INT           NOT NULL CHECK (qs >= 0),
-	prixProd FLOAT         NOT NULL CHECK (prixProd >= 0)
-);
+-- Insertion des évenements
+INSERT INTO Evenement (nomEvent, descEvent, dateEvent, prixEvent) VALUES
+	('Soirée Malevelon Creek', 'Lorem Ipsum', '2001-09-12 18:00:00', 2.0  ),
+	('Promesse des étoiles'  , 'Lorem Ipsum', '2002-10-15 20:00:00', 0.0  ),
+	('Tournoi de poker'      , 'Lorem Ipsum', '2001-03-05 15:15:15', 50.00);
 
--- Création de la table 'Evenement'
-CREATE TABLE Evenement
-(
-	idEvent   SERIAL        PRIMARY KEY,
-	nomEvent  VARCHAR(255)  NOT NULL,
-	dateEvent TIMESTAMP     NOT NULL,
-	prixEvent FLOAT         NOT NULL
-);
-
--- Création de la table "Role"
-CREATE TABLE "Role"
-(
-	nomRole VARCHAR(10) PRIMARY KEY
-);
-
--- Création de la table 'Utilisateur'
-CREATE TABLE Utilisateur
-(
-	netud INT PRIMARY KEY,
-	nom VARCHAR(255) NOT NULL,
-	prenom VARCHAR(255) NOT NULL,
-	tel VARCHAR(10),
-	email VARCHAR(255) NOT NULL,
-	mdp VARCHAR(255) NOT NULL,
-	typeNotification VARCHAR(10) NOT NULL CHECK (typeNotification IN ("Discord", "Mail", "Les deux"))
-	role VARCHAR(10) NOT NULL,
-	demande BOOLEAN NOT NULL,
-	FOREIGN KEY (role) REFERENCES "Role"(nomRole) ON DELETE CASCADE
-);
-
--- Création de la table Commande
-CREATE TABLE Commande
-(
-	netud INT NOT NULL,
-	idProd INT NOT NULL,
-	numCommande INT NOT NULL,
-	qa INT NOT NULL CHECK (qa >= 0),
-
-	FOREIGN KEY (netud ) REFERENCES Utilisateur(netud ) ON DELETE CASCADE,
-	FOREIGN KEY (idProd) REFERENCES Produit    (idProd) ON DELETE CASCADE,
-
-	PRIMARY KEY (netud, idProd)
-);
-
--- Création de la table Inscrit
-CREATE TABLE Inscrit
-(
-	netud INT NOT NULL,
-	idEvent INT NOT NULL,
-	note INT,
-	commentaire VARCHAR(255),
-
-	FOREIGN KEY (netud  ) REFERENCES Utilisateur(netud  ) ON DELETE CASCADE,
-	FOREIGN KEY (idEvent) REFERENCES Evenement  (idEvent) ON DELETE CASCADE,
-
-	PRIMARY KEY (netud, idEvent)
-);
-
--- Création de la table Autorise
-CREATE TABLE Autorise
-(
-	idEvent INT NOT NULL,
-	nomRole VARCHAR(10) NOT NULL,
-
-	PRIMARY KEY (idEvent, nomRole)
-);
-
--- Insertion des rôles
-INSERT INTO "Role"(nomRole) VALUES ('admin'), ('adherent'), ('membre');
-
--- Insertion d'un administrateur
-INSERT INTO Utilisateur(netud, nom, prenom, tel, email, mdp, typeNotification, role, demande) VALUES
-    (1234, 'Test', 'Johnny', '0000000000', 'test@gmail.com', '1234', 'Les deux', 'admin', FALSE);
+-- Insertion des roles autorisés aux évenements
+INSERT INTO Autorise (idEvent, nomRole) VALUES
+	(1, 'admin'   ), -- Autorise Admin
+	(1, 'adherent'), -- Autorise Adhérent
+	(1, 'membre'  ), -- Autorise Membre
+	(2, 'admin'   ),
+	(2, 'adherent'),
+	(3, 'admin'   ),
+	(3, 'adherent');
