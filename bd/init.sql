ea52d8d011730ff2ed5f63de0e6639c492b9f02d
Mangeant Thibault <mangeant.thibault@gmail.com>
2025-04-01 09:38:46 +0200

    Images d'event + Images de produits + initBD

--- bd/init.sql
+++ bd/init.sql
@@ -1,15 +1,23 @@
 -- Insertion des produits
 INSERT INTO Produit (nomProd, qs, prixProd, imgProd) VALUES
-	('Clé USB', 10, 14.99, 'usb.png'),
-	('T-shirt', 35, 39.99, 'tshirt.png'),
-	('Tasse', 20, 7.99, 'tasse.png'),
-	('Gourde', 10, 9.99, 'gourde.png'),
-	('Bloc-note', 25, 8.99, 'bloc-note.png'),
-	('Stylo', 150, 0.99, 'stylo.png'),
-	('Casquette', 30, 29.99, 'casquette.png');
+	('T-shirt', 35, 39.99, 'tshirt.jpeg'),
+	('Tasse', 20, 7.99, 'tasse.jpeg'),
+	('Gourde', 10, 9.99, 'gourde.jpeg'),
+	('Bloc-note', 25, 8.99, 'bloc-note.jpeg'),
+	('Casquette', 30, 29.99, 'casquette.jpeg'),
+	('Chaussons', 30, 29.99, 'chaussons.jpeg'),
+	('Chemise', 25, 24.99, 'chemise.jpeg'),
+	('Hoodie', 50, 44.99, 'hoodie.jpeg'),
+	('Sandales', 20, 44.99, 'sandales.jpeg'),
+	('Tapis de souris', 10, 30.00, 'mousepad.jpeg'),
+	('Pins', 200, 0.99, 'pins.jpeg'),
+	('Sac', 100, 2.99, 'sac.jpeg'),
+	('Coque téléphone', 10, 49.99, 'coquetel.jpeg'),
+	('Teddy bear', 5, 29.99, 'teddybear.jpeg');
+
 
 -- Insertion des évenements
 INSERT INTO Evenement (nomEvent, descEvent, dateEvent, lieuEvent, prixEvent, roleAutoriseMin, imgEvent) VALUES
-	('Soirée Malevelon Creek', 'Lorem Ipsum', '2001-09-12 18:00:00', 'Quelque part', 2.0  , 'membre'  , 'malevelon-creek.png'),
-	('Promesse des étoiles'  , 'Lorem Ipsum', '2002-10-15 20:00:00', 'Un endroit'  , 0.0  , 'adherant', 'promesse-des-etoiles.png'),
-	('Tournoi de poker'      , 'Lorem Ipsum', '2001-03-05 15:15:15', 'Où ça ?'     , 50.00, 'adherant', 'tournoi-poker.png');
+	('Soirée Malevelon Creek', 'Lorem Ipsum', '2001-09-12 18:00:00', 'Quelque part', 2.0  , 'membre'  , 'malevelon-creek.webp'),
+	('Promesse des étoiles'  , 'Lorem Ipsum', '2002-10-15 20:00:00', 'Un endroit'  , 0.0  , 'adherant', 'promesse-des-etoiles.webp'),
+	('Tournoi de poker'      , 'Lorem Ipsum', '2001-03-05 15:15:15', 'Où ça ?'     , 50.00, 'adherant', 'tournoi-poker.jpeg');
