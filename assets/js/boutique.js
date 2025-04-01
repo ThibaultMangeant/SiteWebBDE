bcdabc519e3828bcdbaf732acbbf9bd50203a9d8
Mangeant Thibault <mangeant.thibault@gmail.com>
2025-04-01 13:40:33 +0200

    Rework gerer boutique

--- assets/js/boutique.js
+++ assets/js/boutique.js
@@ -1,46 +1,12 @@
 const groupOptions = document.getElementById("groupOptions");
-const cardFooters  = document.getElementsByClassName("card-body");
 
+const redirectionGererBoutique = document.createElement("a");
+redirectionGererBoutique.href = "produit/produits.php";
 
-const redirectionAjouterProduit = document.createElement("a");
-redirectionAjouterProduit.href = "ajouter_produit.php";
+const btnGererBoutique = document.createElement("button");
+btnGererBoutique.textContent = "Gerer la boutique";
+btnGererBoutique.classList.add("btn");
+btnGererBoutique.classList.add("btn-outline-secondary");
 
-const redirectionModifierProduit = document.createElement("a");
-redirectionModifierProduit.href = "ajouter_produit.php";
-
-const btnAjouterProduit   = document.createElement("button");
-const btnModifierProduit  = document.createElement("button");
-const btnSupprimerProduit = document.createElement("button");
-
-
-// Gestion du style du bouton Ajouter produit
-btnAjouterProduit.textContent = "Ajouter un produit";
-btnAjouterProduit.classList.add("btn");
-btnAjouterProduit.classList.add("btn-outline-secondary");
-
-
-// Gestion du style du bouton Modifier produit
-btnModifierProduit.textContent = "Modifier le produit";
-btnModifierProduit.classList.add("btn");
-btnModifierProduit.classList.add("btn-outline-secondary");
-
-
-// Gestion du style du bouton Supprimer produit
-btnSupprimerProduit.textContent = "Supprimer le produit"
-btnSupprimerProduit.classList.add("btn");
-btnSupprimerProduit.classList.add("btn-outline-secondary");
-
-
-redirectionAjouterProduit.appendChild(btnAjouterProduit);
-redirectionModifierProduit.appendChild(btnModifierProduit);
-groupOptions.appendChild(redirectionAjouterProduit);
-
-
-for (let cardFooter of cardFooters)
-{
-	const optionsModification = document.createElement("div");
-	optionsModification.appendChild(redirectionModifierProduit.cloneNode(true));
-	optionsModification.appendChild(btnSupprimerProduit.cloneNode(true));
-
-	cardFooter.appendChild(optionsModification);
-}
+redirectionGererBoutique.appendChild(btnGererBoutique);
+groupOptions.appendChild(redirectionGererBoutique);
