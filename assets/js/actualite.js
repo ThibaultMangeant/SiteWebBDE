c341074b377a972b0d66954701a34f57c3ab95f6
Mangeant Thibault <mangeant.thibault@gmail.com>
2025-04-02 11:00:06 +0200

    gestionActualite termine cote JS pour de vrai

--- assets/js/actualite.js
+++ assets/js/actualite.js
@@ -3,18 +3,23 @@ const inputDesc  = document.getElementById("inputDesc");
 
 const actualiteCard = document.querySelectorAll(".actualite-card");
 
+
+let cardPrec = null;
 actualiteCard.forEach(card =>
 {
 	card.addEventListener("click", (event) =>
 	{
+		if (cardPrec !== null)
+			cardPrec.classList.remove("selected");
+
+		card.classList.add("selected");
 		const titre = card.querySelector(".actualite-title").textContent;
 
 		const description = card.querySelector(".actualite-desc").textContent;
 
-		console.log("Titre : " + titre);
-		console.log("Description : " + description);
-
 		inputTitre.value = titre;
 		inputDesc.value  = description;
+
+		cardPrec = card;
 	});
 });
