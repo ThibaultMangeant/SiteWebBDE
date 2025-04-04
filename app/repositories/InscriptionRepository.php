26b5e2959b01123882fb0601eb2177f9e36a2284
Julien Oyer <oyer.ludovic@gmail.com>
2025-04-04 10:53:59 +0200

    Suppression des méthodes findByUtilisateur, deleteByUtilisateur et deleteByEvenement dans InscriptionRepository pour simplifier le code.

--- app/repositories/InscriptionRepository.php
+++ app/repositories/InscriptionRepository.php
@@ -21,15 +21,6 @@ class InscriptionRepository
 		return $inscriptions;
 	}
 
-	public function findByUtilisateur($idUtilisateur): ?Inscription
-	{
-		$stmt = $this->pdo->prepare('SELECT * FROM Inscrit WHERE netud = :idUtilisateur');
-		$stmt->bindParam(':idUtilisateur', $idUtilisateur);
-		$stmt->execute();
-		$row = $stmt->fetch(PDO::FETCH_ASSOC);
-		return $row ? $this->createInscriptionFromRow($row) : null;
-	}
-
 	public function findByEvenement($idevenement): array
 	{
 		$stmt = $this->pdo->prepare('SELECT * FROM Inscrit WHERE idEvent = :idevenement');
@@ -60,32 +51,10 @@ class InscriptionRepository
 		return (int) $stmt->fetchColumn();
 	}
 
-	public function deleteByUtilisateurAndEvenement($idUtilisateur, $idEvenement): bool
-	{
-		$stmt = $this->pdo->prepare('DELETE FROM Inscrit WHERE netud = :idUtilisateur AND idEvent = :idEvenement');
-		$stmt->bindParam(':idUtilisateur', $idUtilisateur);
-		$stmt->bindParam(':idEvenement', $idEvenement);
-		return $stmt->execute();
-	}
-
 	public function deleteAvisByUtilisateurAndEvenement($idUtilisateur, $idEvenement) : bool
 	{
 		$stmt = $this->pdo->prepare('UPDATE Inscrit SET note=0, commentaire=NULL WHERE netud = :idUtilisateur AND idEvent = :idEvenement');
 		$stmt->bindParam(':idUtilisateur', $idUtilisateur);
-		$stmt->bindParam(':idEvenement', $idEvenement);
-		return $stmt->execute();
-	}
-
-	public function deleteByUtilisateur($idUtilisateur): bool
-	{
-		$stmt = $this->pdo->prepare('DELETE FROM Inscrit WHERE netud = :idUtilisateur');
-		$stmt->bindParam(':idUtilisateur', $idUtilisateur);
-		return $stmt->execute();
-	}
-
-	public function deleteByEvenement($idEvenement): bool
-	{
-		$stmt = $this->pdo->prepare('DELETE FROM Inscrit WHERE idEvent = :idEvenement');
 		$stmt->bindParam(':idEvenement', $idEvenement);
 		return $stmt->execute();
 	}
